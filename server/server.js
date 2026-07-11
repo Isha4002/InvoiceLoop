842be05a18b141bc1e85c88d6392cfd48f44ad34
Isha Pal <ishapal2004@gmail.com>
2026-07-11 21:23:18 +0530

    Add user model and authentication routes

--- server/server.js
+++ server/server.js
@@ -3,18 +3,24 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.json({
     success: true,
-    message: "InvoiceLoop API Running 🚀"
+    message: "InvoiceLoop API Running 🚀",
   });
 });
 
